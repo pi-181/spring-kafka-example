e071b27130ed3e71276f63e8f2d3be54767ba63c
Max Demydenko <m_demy@pm.me>
2022-11-20 22:58:42 +0200

    feat: initial commit

---
+++ src/main/java/com/demkom58/spring_kafka_example/SpringKafkaExampleApplication.java
@@ -0,0 +1,9 @@
+package com.demkom58.spring_kafka_example;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringKafkaExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaExampleApplication.class, args);
+    }
+}
